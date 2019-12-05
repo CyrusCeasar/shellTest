1f8a60d8254d2deebee02f868311a7508eeb102c
cyrus <cyruschan.k@gmail.com>
2019-12-05 15:29:33 +0800

    test command

---
+++ autopush.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+. $(dirname "$0")/colors.sh
+PROJECT_PATH="/var/www/personalservice"
+FILE_NAME="translate_record.sqlite3"
+cd $PROJECT_PATH
+pwd
+RESULT=$(git diff master:$FILE_NAME $FILE_NAME)
+if [  ${#RESULT} -eq 0 ]; then
+  echo "true"
+  git commit -m "auto commit" $FILE_NAME && git push original master
+	echo -e "${Red} commit success"
+else
+  echo "false"
+fi
+
+
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+. $(dirname "$0")/colors.sh
+
+set
+echo -e "${Red} this is bow"
